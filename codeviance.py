935476cddf822da3f1d78b0242b13a80991a983e
Simon Bihel <simon.bihel@outlook.com>
2015-10-16 11:29:00 +0200

    Changing tabs to spaces

--- codeviance.py
+++ codeviance.py
@@ -1,18 +1,16 @@
 import numpy as np
 
-def average(X):
-	n = 0
-	S = 0
-	for e in X:
-		S = S + e
-		n += 1
-	return S / n
 
+def average(X):
+    n = 0
+    S = 0
+    for e in X:
+        S = S + e
+        n += 1
+    return S / n
 
 
 def codeviance(X, Y):
-	av_x = average(X)
-	av_y = average(Y)
-	return average (X*Y) - (av_x * av_y)
-	
-
+    av_x = average(X)
+    av_y = average(Y)
+    return average(X*Y) - (av_x * av_y)
