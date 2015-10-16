4a9a482304f4f9df06e99c7deb17e7b6c1bb16af
florestan.demoor <florestan.demoor@gmail.com>
2015-10-16 11:12:16 +0200

    [codeviance] Codeviance working

--- codeviance.py
+++ codeviance.py
@@ -1,1 +1,18 @@
+import numpy as np
+
+def average(X):
+	n = 0
+	S = 0
+	for e in X:
+		S = S + e
+		n += 1
+	return S / n
+
+
+
+def codeviance(X, Y):
+	av_x = average(X)
+	av_y = average(Y)
+	return average (X*Y) - (av_x * av_y)
+	
 
---
+++ tests.py
@@ -0,0 +1,6 @@
+from codeviance import *
+	
+X = np.array([2, 4], dtype = float)
+Y = np.array([0, 1], dtype = float)
+
+print(codeviance(X, Y))
