5038a7f2704e39943779078aa9118ffd6f249f2d
florestan.demoor <florestan.demoor@gmail.com>
2015-10-18 23:15:20 +0200

    Doing some tests

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 from computing import *
+import extract_sdschttp as sdsc
+import extract_epahttp as epa
+import extract_calgaryaccess as calga
 
 
 X = [randint(0, 50) for i in range(50)]
@@ -13,3 +16,13 @@ DATA = [X, Y,Z]
 
 cod_matrix = codeviance_all_streams(DATA, eps, delta, u)
 print(cod_matrix)
+
+
+#data_stream = calga.extract_filename()
+#data_stream = calga.extract_sources()
+data_stream = epa.extract_sources()
+#data_stream = sdsc.extract_sources(1)
+#data_stream = sdsc.extract_sources(2)
+
+#print(data_stream)
+print(len(data_stream))
