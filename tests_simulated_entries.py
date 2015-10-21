7d99cadf1e846e84233de2b4b052319488335473
florestan.demoor <florestan.demoor@gmail.com>
2015-10-21 17:04:19 +0200

    3D plotting seems to work with log scale

--- tests_simulated_entries.py
+++ tests_simulated_entries.py
@@ -2,6 +2,10 @@ from computing import *
 from traces_extraction import *
 from distribution import *
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.mlab import griddata
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
 
 size = 1000
@@ -31,23 +35,44 @@ def print_data(data):
     data_array = np.array([e for e in data])
     plt.hist(data_array)
     plt.show()
+    return None
 
-print_data(data0)
+#print_data(data12)
 
 
 
-#DATA = [globals()['data' + str(i)] for i in range(13)]
+DATA = [globals()['data' + str(i)] for i in range(13)]
 
 
 ### SKECH MIN RESULTS
 
 #cod_matrix = codeviance_all_streams(DATA, eps, delta, u)
 
-### REAL CODEVIANCE MATRIX
+## REAL CODEVIANCE MATRIX
 
-#cod_real_matrix = np.array([[0. for i in range(13)] for j in range(13)], dtype=float)
-#for i in range(13):
-        #for j in range(i,13):
-            #cod = codeviance(DATA[i], DATA[j])
-            #cod_real_matrix[i,j] = cod
-            #cod_real_matrix[j,i] = cod
+cod_real_matrix = np.array([[0. for i in range(13)] for j in range(13)], dtype=float)
+for i in range(13):
+        for j in range(i,13):
+            cod = codeviance(np.array([e for e in DATA[i]]), np.array([e for e in DATA[j]]))
+            cod_real_matrix[i,j] = cod
+            cod_real_matrix[j,i] = cod
+
+
+Xi = np.array([i for i in range(13)])
+Yi = np.array([i for i in range(13)])
+
+x = np.array([j for i in range(13) for j in range(13)])
+y = np.array([i for i in range(13) for j in range(13)])
+z = np.array([cod_real_matrix[i,j] for i in range(13) for j in range(13)])
+mini = min_vect(z)
+for k in range(169):
+        z[k] = np.log(z[k] + abs(mini) + 1)
+
+X, Y = np.meshgrid(Xi, Yi)
+
+Z = griddata(x, y, z, X, Y)
+
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1)
+plt.show()
+
+print(cod_real_matrix)
